203b479eae6130f59e1797d3ae5e0274343e1302
jecontreras68 <joseeduar147@gmail.com>
2025-07-14 12:17:13 -0500

    cambios nuevos

---
+++ api/controllers/EstadoDespachoController.js
@@ -0,0 +1,27 @@
+module.exports = {
+    createEstado: async function (req, res) {
+      try {
+        const { fecha, descripcion, transporte } = req.body;
+  
+        const nuevoEstado = await EstadoDespacho.create({
+          fecha,
+          descripcion,
+          transporte
+        }).fetch();
+  
+        return res.json(nuevoEstado);
+      } catch (err) {
+        return res.serverError(err);
+      }
+    },
+  
+    listar: async function (req, res) {
+      try {
+        let params = req.allParams();
+        const estados = await EstadoDespacho.find( params ).sort('fecha DESC');
+        return res.json(estados);
+      } catch (err) {
+        return res.serverError(err);
+      }
+    }
+  };
---
+++ api/models/EstadoDespacho.js
@@ -0,0 +1,29 @@
+/**
+ * EstadoDespacho.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+module.exports = {
+  attributes: {
+    fecha: {
+      type: 'ref',
+      columnType: 'datetime',
+      required: true,
+    },
+
+    descripcion: {
+      type: 'string',
+      required: true,
+    },
+
+    transporte: {
+      type: 'string',
+      allowNull: true,
+    },
+    idVventa:{
+      model: 'tblventas',
+      required: true
+    }
+  },
+};
